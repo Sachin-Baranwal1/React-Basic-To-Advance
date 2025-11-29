9825ed99d98682ba7d3e2b7abc4e7b8f33500030
Sachin Baranwal <sachinbaranawal2004@gmail.com>
2025-11-30 02:13:25 +0530

    Updation

--- 10-CoinFlipProject/src/assets/Counter.jsx
+++ 10-CoinFlipProject/src/assets/Counter.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Counter = (props) => {
   return (
-      <div className="flex w-full text-4xl  justify-center items-center h-[25%] px-3 gap-10 ">
-        <div className="font-semibold">
+      <div className="flex w-full md:text-4xl  text-2xl justify-center items-center h-[25%] px-3 md:gap-10  ">
+        <div className="font-semibold pr-5">
           Heads: <span className="text-white">{
           props.heads} </span>
         </div>
